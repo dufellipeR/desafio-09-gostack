8903a3fbe2d4cdf851df09f968c0e8dcf2506a86
dufellipeR <eduardo.fellipe30@gmail.com>
2020-12-14 11:53:24 -0300

    Customers Ready

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    let customer = await this.customersRepository.findByEmail(email);
+
+    if (customer) {
+      throw new AppError('This e-mail is already assigned to a customer');
+    }
+
+    customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
